63fabbc5eb3961439e7297b2374ff8362e9db2ce
ChawkiMbarki <chawkimbarki500@gmail.com>
2024-09-11 13:51:39 -0700

    Updated dependencies, fixed typos + minor changes

--- Server/routes/project.routes.js
+++ Server/routes/project.routes.js
@@ -1,19 +1,9 @@
-const express = require('express');
-const {
-  getAllProjects,
-  getProjectById,
-  createProject,
-  updateProject,
-  deleteProject
-} = require('../controllers/projectController');
+const projectController = require("../controllers/project.controller");
 
-const router = express.Router();
-
-// Routes
-router.get('/projects', getAllProjects);
-router.get('/projects/:id', getProjectById);
-router.post('/projects', createProject);
-router.put('/projects/:id', updateProject);
-router.delete('/projects/:id', deleteProject);
-
-module.exports = router;
+module.exports = function (app) {
+  app.get("/api/projects", projectController.getAllProjects);
+  app.get("/api/projects/:id", projectController.getProjectById);
+  app.post("/api/projects", projectController.createProject);
+  app.put("/api/projects/:id", projectController.updateProject);
+  app.delete("/api/projects/:id", projectController.deleteProject);
+};
