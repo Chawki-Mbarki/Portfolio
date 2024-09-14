2d3405ea1f3794e0b1ab63107c1e5a6956cecd9e
ChawkiMbarki <chawkimbarki500@gmail.com>
2024-09-14 16:10:41 -0700

    Updated Routes for better consistency

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -1,10 +1,14 @@
 const userController = require('../controllers/user.controller');
 
 module.exports = (app) => {
-  app.post('/api/register', userController.registerUser);
-  app.post('/api/login', userController.loginUser);
-  app.post('/api/logout', userController.logoutUser);
-  app.get('/api/protected', userController.authenticate, (req, res) => {
-    res.status(200).json({ message: 'Access granted' });
+  app.get('/api/users', userController.getAllUsers);
+  app.get('/api/users/:id', userController.getUserById);
+  app.put('/api/users/:id', userController.updateUser);
+  app.delete('/api/users/:id', userController.deleteUser);
+  app.post('/api/users/register', userController.registerUser);
+  app.post('/api/users/login', userController.loginUser);
+  app.post('/api/users/logout', userController.logoutUser);
+  app.get("/api/users/protected", userController.authenticate, (req, res) => {
+    res.status(200).json({ message: "Access granted" });
   });
 };
