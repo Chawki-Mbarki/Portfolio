72cf11192cad2a689591daeacd3ccf313bf95d32
ChawkiMbarki <chawkimbarki500@gmail.com>
2024-09-14 16:10:20 -0700

    Simplified user model and added email validation

--- Server/models/user.model.js
+++ Server/models/user.model.js
@@ -1,28 +1,35 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true, trim: true },
   lastName: { type: String, required: true, trim: true },
-  email: { type: String, required: true, unique: true, trim: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    validate: {
+      validator: (val) => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
+      message: "Please enter a valid email",
+    },
+  },
   password: { type: String, required: true },
 });
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
+
+  try {
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 userSchema.methods.comparePassword = function (password) {
   return bcrypt.compare(password, this.password);
-};
-
-userSchema.methods.generateAuthToken = function () {
-  return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
-    expiresIn: "1h",
-  });
 };
 
 const User = mongoose.model("User", userSchema);
